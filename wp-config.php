74daae752c3f96c57a5db5e27b6eadad36f548ae
adji142 <prasetyoajiw@gmail.com>
2021-12-07 22:43:49 +0700

    day 9.1

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'webmask' ); // comk5683_ renungan_lm
+define( 'DB_NAME', 'comk5683_lumina2' ); // comk5683_ renungan_lm
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'comk5683_root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'hsp123' );
+define( 'DB_PASSWORD', 'lagis3nt0s4' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
